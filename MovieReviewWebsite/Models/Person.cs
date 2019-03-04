7722d19ef97b4056ae931c3f114d23af48e77559
Mathteaus <mofficial1234@gmail.com>
2019-03-04 10:58:14 +0000

    Added working people

---
+++ MovieReviewWebsite/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+namespace MovieReviewWebsite.Models
+{
+    [Table("persons")]
+    public class Person
+    {
+        [Key]
+        public virtual int personID { get; set; }
+        public virtual string personName { get; set; }
+        public virtual string personSurname { get; set; }
+        public virtual DateTime dateOfBirth { get; set; }
+        public virtual string personMovies { get; set; }
+        public virtual string personRole { get; set; }
+        //public virtual int CategoryId { get; set; }
+        //public virtual Category Category { get; set; }
+    }
+}
