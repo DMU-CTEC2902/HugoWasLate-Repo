e23021cc9480cbda808a150decbfdacbe8ff533b
p17238710 <p17238710@my365.dmu.ac.uk>
2019-03-05 15:31:40 +0000

    deleted category

--- MovieReviewWebsite/Models/MovieDataInitialiser.cs
+++ MovieReviewWebsite/Models/MovieDataInitialiser.cs
@@ -10,34 +10,26 @@ namespace MovieReviewWebsite.Models
     {
         protected override void Seed(MovieContext context)
         {
-            Category cat1 = new Category();
-            cat1.CategoryId = 1;
-            cat1.Name = "Comedy";
-            cat1.Description = "Music compact discs";
-            context.Categories.Add(cat1);
-
-            Category cat2 = new Category();
-            cat2.CategoryId = 2;
-            cat2.Name = "Horror";
-            cat2.Description = "Film DVDs";
-            context.Categories.Add(cat2);
+            
 
             Movie prod1 = new Movie();
             prod1.MovieID = 1;
-            prod1.CategoryId = 1;
+           
             prod1.MovieName = "Cars";
             prod1.Description = "Animated movie";
-            prod1.Category = cat1;
+            prod1.CategoryName = "Horror";
             prod1.Rating = 3.0f;
+            prod1.personID = 1;
             context.Movies.Add(prod1);
 
             Movie prod2 = new Movie();
             prod2.MovieID = 2;
-            prod2.CategoryId = 2;
+            
             prod2.MovieName = "Fast and Furious 261";
             prod2.Description = "More terrible car wrecks";
-            prod2.Category = cat2;
+            prod2.CategoryName = "Horror";
             prod2.Rating = 7.0f;
+            prod2.personID = 2;
             context.Movies.Add(prod2);
 
             base.Seed(context);
